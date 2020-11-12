a989f5e9c99bdd51551d65e3e353d563d59607a1
Fatima <fatimaolasunkanmi01@gmail.com>
2020-11-12 17:48:46 +0100

    project folder created

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import React from 'react';
 import './App.css';
-import NavBar from './components/NavBar'
-import Home from './components/Home'
-// import About from './components/About'
 
-// import Contact from './components/Contact'
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <Home />
-      {/* <About />
-     
-      <Contact /> */}
-
+    <div >
+      
     </div>
   );
 }
 
 export default App;
+
