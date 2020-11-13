9520e7081244f57943c7915b8362eda07eb0ace4
Fatima <fatimaolasunkanmi01@gmail.com>
2020-11-13 06:18:48 +0100

    who we are section added to landing component

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -8,6 +8,25 @@ const Landing =()=> {
         <div>
             <Navigation/>
             <HeroSection/>
+            <div className="who-we-are">
+                <h4 className="center-align">Who we are</h4>
+               <div className="who-we-are-section">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa velit repudiandae blanditiis fuga, eum adipisci earum 
+                        numquam veritatis, reiciendis vel autem recusandae molestias deleniti dicta. Dolore totam fugit inventore laborum.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa velit repudiandae blanditiis fuga, eum adipisci earum 
+                        numquam veritatis, reiciendis vel autem recusandae molestias deleniti dicta. Dolore totam fugit inventore laborum.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa velit repudiandae blanditiis fuga, eum adipisci earum 
+                        numquam veritatis, reiciendis vel autem recusandae molestias deleniti dicta. Dolore totam fugit inventore laborum.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa velit repudiandae blanditiis fuga, eum adipisci earum 
+                        numquam veritatis, reiciendis vel autem recusandae molestias deleniti dicta. Dolore totam fugit inventore laborum.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa velit repudiandae blanditiis fuga, eum adipisci earum 
+                        numquam veritatis, reiciendis vel autem recusandae molestias deleniti dicta. Dolore totam fugit inventore laborum.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa velit repudiandae blanditiis fuga, eum adipisci earum 
+                        numquam veritatis, reiciendis vel autem recusandae molestias deleniti dicta. Dolore totam fugit inventore laborum.
+                    </p>
+                    <img src="/team.png" alt="team" />
+               </div>
+            </div>
         </div>
     )
 }
