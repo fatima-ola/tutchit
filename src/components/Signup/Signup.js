d01662f444331b0f4c9ea36b5bb62fb091b357ab
Fatima <fatimaolasunkanmi01@gmail.com>
2020-11-20 21:59:45 +0100

    firebase authentication added

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -10,14 +10,41 @@ const Signup =()=> {
     const [password, setPassword] = useState('');
     const [errorEmail, setErrorEmail] = useState('');
     const [errorPassword, setErrorPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const handleChange = (e) => {
+        const {name, value} = e.currentTarget;
+        if(name === 'fullname'){
+            setFullname(value);
+        }else if(name === 'email'){
+            setEmail(value);
+        }else if(name === 'password'){
+            setPassword(value);
+        }
+    }
+
+
+    const handleSubmit = (e) => {
+       e.preventDefault;
+    
+       try {
+           if(!fullname || !email || !password){
+            setErrorMessage('All Fields are Required');
+           }
+       } catch (error) {
+           
+       }
+    }
+
     return (
         <div className="container">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="signup">
                     <NavLink to="/" className="signin-title">TutChit</NavLink>
-                    <TextInputSection placeholder="Enter Your Full Name" type="text" label="Full Name"/>
-                    <TextInputSection placeholder="Enter Email Address" type="email" label="Email Address"/>
-                    <TextInputSection  placeholder="Enter Your Password" type="password" label="Password"/>
+                        {errorMessage? <p>{errorMessage}</p> : ''}
+                    <TextInputSection placeholder="Enter Your Full Name" type="text" label="Full Name" value={fullname} name="fullname" handleChange={handleChange}/>
+                    <TextInputSection placeholder="Enter Email Address" type="email" label="Email Address" value={email} name="email" handleChange={handleChange}/>
+                    <TextInputSection  placeholder="Enter Your Password" type="password" label="Password" value={password} name="password" handleChange={handleChange}/>
                     <ButtonSection text="Signup" className="buttonSignup blue"/>
                     <p className="center-align">Already have an account? <a href="/login">Signin</a></p>
                 </div>
--- src/components/TextInput/TextInputSection.js
+++ src/components/TextInput/TextInputSection.js
@@ -2,13 +2,16 @@ import React from 'react'
 import {TextInput} from 'react-materialize';
 
 const TextInputSection =(props)=> {
-    const {placeholder, type, label} = props;
+    const {placeholder, type, label, handleChange, name, value} = props;
     return (
         <div>
             <label>{label}</label>
             <TextInput placeholder={placeholder}
                 type={type}
                 inputClassName="browser-default"
+                name={name}
+                value={value}
+                onChange={handleChange}
                 />
         </div>
     )
